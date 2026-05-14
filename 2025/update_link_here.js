88e9a7406406c1621060ec65ded92af73e0197e7
Tasks Unlimited <cluhmann@tasksunlimited.org>
2026-05-14 09:56:30 -0500

    Create update_link_here.js

---
+++ 2025/update_link_here.js
@@ -0,0 +1,1 @@
+const MASTER_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQr6wegUTldLyoaq7qifjk7u3mAW0ixZkpCrSsqu5-WAr53OY-WcGHH1d9grbU7lUCmQ8HElBoh5FRj/pub?output=csv&gid=428293558";
